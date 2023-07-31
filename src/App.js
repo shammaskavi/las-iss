c783e819aecad856040c36157f86925ca9678016
shammaskavi <shammaskavi@gmail.com>
2023-07-31 11:30:02 +0530

    Starting of the end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import DataDisplay from "./DataDisplay"; // Update this path based on your project structure
+import "./App.css";
+import HomePage from "./HomePage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div>
+    //   <center>
+    //     <Link to="/">
+    //       <img
+    //         className="Header-img"
+    //         src="https://cdn.discordapp.com/attachments/1121100423818907739/1134483528688603227/google-form_header.png"
+    //         alt="Laurel shit"
+    //         height={200}
+    //       />
+    //     </Link>
+    //     <Switch>
+    //       <Route path="/data/:id" Component={DataDisplay} />
+    //     </Switch>
+    //   </center>
+    // </div>
+
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">
+              <img
+                className="Header-img"
+                src="https://cdn.discordapp.com/attachments/1121100423818907739/1134483528688603227/google-form_header.png"
+                alt="Laurel shit"
+                height={200}
+              />
+            </Link>
+          </li>
+          {/* Add other navigation links if needed */}
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/data/:id" component={DataDisplay} />
+        {/* Add other routes if needed */}
+      </Switch>
+      {/* <DataDisplay /> */}
+      <HomePage />
     </div>
   );
-}
+};
 
 export default App;
